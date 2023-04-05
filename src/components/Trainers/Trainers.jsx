8dc57be78c649ac2f7356ccc377f683569019c80
shhady <shhadyse@gmail.com>
2023-04-05 11:49:02 +0300

    clean comment

--- src/components/Trainers/Trainers.jsx
+++ src/components/Trainers/Trainers.jsx
@@ -25,32 +25,6 @@ export default function Trainers() {
             </div>
             </Link>
         ))}
-{/*         
-        <div>
-            <img src="https://images.unsplash.com/photo-1632781297772-1d68f375d878?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OTN8fGd5bXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60" alt="" width="200px" height={200} style={{borderRadius:"50%"}} priority={String(true)} />
-            <h3>Trainer Name</h3>
-            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Autem, aspernatur!</p>
-        </div>
-        <div>
-            <img src="https://images.unsplash.com/photo-1584863265045-f9d10ca7fa61?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8ODd8fGd5bXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60" alt="" width="200px" height={200} style={{borderRadius:"50%"}} priority={String(true)} />
-            <h3>Trainer Name</h3>
-            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Autem, aspernatur!</p>
-        </div>
-        <div>
-            <img src="https://images.unsplash.com/photo-1627197843575-00cc3965c2d5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OTh8fGd5bXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60 " alt="" width="200px" height={200} style={{borderRadius:"50%"}} priority={String(true)} />
-            <h3>Trainer Name</h3>
-            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Autem, aspernatur!</p>
-        </div>
-        <div>
-            <img src="https://images.unsplash.com/photo-1632781297772-1d68f375d878?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OTN8fGd5bXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60" alt="" width="200px" height={200} style={{borderRadius:"50%"}} priority={String(true)} />
-            <h3>Trainer Name</h3>
-            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Autem, aspernatur!</p>
-        </div>
-        <div>
-            <img src="https://images.unsplash.com/photo-1627197843575-00cc3965c2d5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OTh8fGd5bXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60 " alt="" width="200px" height={200} style={{borderRadius:"50%"}} priority={String(true)} />
-            <h3>Trainer Name</h3>
-            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Autem, aspernatur!</p>
-        </div> */}
     </div>
     </div>
   )
